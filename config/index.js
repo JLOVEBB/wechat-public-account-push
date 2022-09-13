116ef77460f21c5d2a80dc055ae00529f7fdfd9e
JLOVEBB <112044226+JLOVEBB@users.noreply.github.com>
2022-09-13 13:33:20 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -27,7 +27,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oJg1R6cpfgj9K5ffapEU2A7UAbf4",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "Mjizz1XqnhrJaOPmCLJBc0OAxGYrBltoO_fo1__GQ18",
+      useTemplateId: "4GF900BcvywbRd1jf6d4fmQ86-qAkEmVN2lAXCTo70Q",
       // 所在省份
       province: "浙江",
       // 所在城市
